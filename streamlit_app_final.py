0197f5d2155b528b83c0f45ef52edef0bd36e61b
chrishenryfla <chrishenryfla@ucla.edu>
2023-08-30 00:34:07 -0700

    streamlit_app_final.py

--- streamlit_app_final.py
+++ streamlit_app_final.py
@@ -10,6 +10,8 @@ def intro_page():
     st.write("This app focuses on developiong the ability to use flavor metrics to predict coffee quality to increase the profitablity of crops.")
     st.write("The global coffee industry was valued at 466 Billion dollars in 2020 and is forecasted to continually increase into 2026.")
     st.write("Coffee quality can be determined using a variety of features. This tool uses data from the [Coffee Quality Institute](https://database.coffeeinstitute.org) and machine learning to make predictions of overall quality, deemed 'Total Cup Points', from a reduced amount of features or 'Cupping Scores'.")
+    st.image("dot_plot.png", use_column_width=True)
+
     st.markdown("### Data")
     st.write("The data that I am using includes more than 1300 occurrences across 8 quality metrics for coffee across continents. The 8 metrics have been reduced to 4 avoiding multicollinearity and overfitting while increasing interpretability and generalization of the model.")
     st.write("- From these 4 numeric metrics, all combinations of 2 and 3 can be used with an associated country name to make reliable predictions on new unseen data with 0.341-0.377 root mean squared error after an 80/20 train test split.")
